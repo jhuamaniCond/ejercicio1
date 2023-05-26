d71a9cc4482d82a8c3b2019094bcbb66c8493645
jhuamaniCond <134736830+jhuamaniCond@users.noreply.github.com>
2023-05-26 14:43:45 -0500

    nombre

--- ejercicio1.java
+++ ejercicio1.java
@@ -2,6 +2,7 @@ public class HolaMundo{
   public static void main(String args[]){
     system.out.println("hola mundo");
     system.out.println("bienvenida al curso");
+    system.out.println("nombre: Huamani Condori Jeanpiero Sixto");
   }
 
 }
